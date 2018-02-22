ca7a4337b0fa6d606e76cbae2b31d3f097722417
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-22 11:27:39 -0700

    Write fetch call for single pokemon

--- src/helper.js
+++ src/helper.js
@@ -12,3 +12,18 @@ export const fetchPokemon = async () => {
     throw error
   }
 }
+
+export const fetchSinglePokemon = async (pokeId) => {
+  try {
+    const url = `http://localhost:3001/pokemon/${pokeId}`
+    const response = await fetch(url);
+    if (response.status > 226) {
+      throw new Error('we could not find your pokemon :(')
+    } else {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+  } catch (error) {
+    throw error;
+  }
+}
