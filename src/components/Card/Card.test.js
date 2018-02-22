ec3b17e837130471939b4033bf8a9145f79da37a
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-22 11:12:34 -0700

    Update snapshots and update Card test

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -3,8 +3,34 @@ import { shallow } from 'enzyme';
 import Card from './Card';
 
 describe('Card', () => {
+  let renderedComponent;
+  let mockPokemon;
+
+  beforeAll(() => {
+    mockPokemon = {
+      "id": "1",
+      "name": "normal",
+      "pokemon": [
+        "16",
+        "17",
+        "18",
+        "19",
+        "20"
+      ]
+    };
+  })
+
+  beforeEach(() => {
+    renderedComponent = shallow(
+      <Card 
+        pokemon={mockPokemon}
+        key={mockPokemon.id}
+      />
+    )
+  })
+
   it('should match snapshot', () => {
-    const renderedComponent = shallow(<Card />)
+    
 
     expect(renderedComponent).toMatchSnapshot();
   })
