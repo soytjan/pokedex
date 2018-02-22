42ed6023fe37c17782b38c9f3eaff2523c6dcd0b
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-22 09:59:53 -0700

    Set up addPokemon action and set up pokemonReducer

---
+++ src/reducers/pokemonReducer.js
@@ -0,0 +1,10 @@
+const pokemonReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_POKEMON':
+      return [...state, ...action.pokemon]
+    default: 
+      return state;
+  }
+}
+
+export default pokemonReducer;
