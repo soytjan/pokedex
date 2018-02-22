54c499e2e21fffac71ca07798f54efbc41e8e7df
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-22 13:57:59 -0700

    Get more details rendering on the page when card is toggled

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Card.css';
 
-// need to pass down a handleClick function from CardDeck
-// need to check and see isOpened = true || false
-
-
 const Card = ({pokemon, onClick}) => {
+  const moreDetails = pokemon.pokemon.map(pokemon => {
+    const img = pokemon.sprites ? pokemon.sprites.back_default : null;
+    return (
+      <div>
+        <h3>{pokemon.name}</h3>
+        <h3>{pokemon.weight}</h3>
+        <img src={img} alt={`pic of ${pokemon.name}`}/>
+      </div>
+    )
+  })
+  const renderedDetails = pokemon.isSelected ? moreDetails : null;
+
   return (
     <article className='Card' onClick={() =>onClick(pokemon)}>
-      <h3>{pokemon.name}</h3> 
+      <h2>{pokemon.name}</h2>
+      {renderedDetails} 
     </article>
   )
 }
