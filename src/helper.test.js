35603d1d80cf0f6fed71b4b068cdc65bb3cff51b
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-22 11:31:33 -0700

    Write tests for getSinglePokemon fetch call

--- src/helper.test.js
+++ src/helper.test.js
@@ -39,4 +39,43 @@ describe('helper', () => {
       expect(response).rejects.toEqual(expected);
     });
   })
+
+  describe('fetchSinglePokemon', () => {
+    beforeAll(() => {
+      window.fetch = jest.fn().mockImplementation( () => Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve({
+          object: 'poke obj'
+        })
+      }))
+    });
+
+    it('should call fetch with the expected params', () => {
+      const url = 'http://localhost:3001/pokemon/1';
+
+      expect(window.fetch).not.toHaveBeenCalled()
+
+      helper.fetchSinglePokemon(1)
+
+      expect(window.fetch).toHaveBeenCalledWith(url);
+    });
+
+    it('should return an object if status code is ok', () => {
+      const expected = { object: 'poke obj' };
+      const response = helper.fetchSinglePokemon(1);
+
+      expect(response).resolves.toEqual(expected);
+    });
+
+    it('should throw an error if status code is not ok', () => {
+      window.fetch = jest.fn().
+        mockImplementation(() => Promise.resolve({ status: 500 }
+        )
+      )
+      const expected = Error('we could not find your pokemon :(');
+      const response = helper.fetchSinglePokemon(1);
+
+      expect(response).rejects.toEqual(expected);
+    });
+  })
 })
