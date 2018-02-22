e1fa7885440c5d2557841cb69c3e36326183050e
Amanda Tjan <amanda.tjan@gmail.com>
2018-02-22 10:50:23 -0700

    Move gif into src folder, put gif into CardDeck when it is loading

--- src/containers/CardDeck/CardDeck.js
+++ src/containers/CardDeck/CardDeck.js
@@ -3,16 +3,33 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addPokemon } from '../../actions';
 import { fetchPokemon } from '../../helper';
+import pikachu from '../../loading.gif';
 import './CardDeck.css';
 
 export class CardDeck extends Component {
-  componentDidMount = async () => {
-    const pokemon = await fetchPokemon();
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+    }
+  }
 
+  componentDidMount = async () => {
+    this.setState({ isLoading: true })
+    const pokemon = await fetchPokemon();
     this.props.addPokemon(pokemon);
+    this.setState({ isLoading: false })
   }
 
   render() {
+    if (this.state.isLoading) {
+      return (
+        <div>
+          <img src={pikachu} alt="pikachu loading gif"/>
+        </div>
+      )
+    }
+
     return (
       <section className='CardDeck'>
         I'm the CardDeck!
